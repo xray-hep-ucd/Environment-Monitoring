fd0d1037ac265589b410c63ca9020c407f57475b
X-Ray <xray.hep.ucd@outlook.com>
2022-04-18 18:48:34 -0700

    Working 4-rtd readout

--- rtds/multiRTD/multiRTD.ino
+++ rtds/multiRTD/multiRTD.ino
@@ -2,13 +2,11 @@
  * Read and store temperature information from multiple adafruit MAX31865 RTD sensor amplifiers
  * 
  * Based off Adafruit max31865 example code
- * 
- * 
+ *
  */
 
 #include <Adafruit_MAX31865.h>
 #include <TimeLib.h>
-#include <DS1307RTC.h>
 
 // Use software SPI: CS, DI, DO, CLK
 //Adafruit_MAX31865 thermo_1 = Adafruit_MAX31865(10, 11, 12, 13);
@@ -16,7 +14,7 @@
 //Adafruit_MAX31865 thermo_1 = Adafruit_MAX31865(10);
 
 //Current RTDs
-//RTDs share DI, DO, CLK pins but have different CS pins
+//RTDs have different CS pins but share DI, DO, CLK pins 
 Adafruit_MAX31865 thermo_1 = Adafruit_MAX31865(10, 11, 12, 13);
 Adafruit_MAX31865 thermo_2 = Adafruit_MAX31865(9, 11, 12, 13);
 Adafruit_MAX31865 thermo_3 = Adafruit_MAX31865(8, 11, 12, 13);
@@ -30,13 +28,10 @@ void setup()
 {
   Serial.begin(115200); //NB: Serial monitor must match this value or will display unintelligble symbols
   while(!Serial){ } //Wait until serial monitor has started
-  
-  Serial.println("Beginning Multi-RTD Readout");
-  time_t initTime = now();
-  Serial.print("Year="); Serial.println(year(t));
-  Serial.print("Month="); Serial.println(month(t)); 
-  Serial.print("Day="); Serial.println(day(t));
+  delay(2000);
 
+  Serial.println("Beginning Multi-RTD Readout");
+  
   //Initialize the RTDs as the 2-wire variety
   thermo_1.begin(MAX31865_2WIRE); 
   thermo_2.begin(MAX31865_2WIRE);
@@ -47,6 +42,7 @@ void setup()
 void loop()
 {
   Serial.println();
+  
   //Read in temps from the sensors
   float temp_1 = thermo_1.temperature(RNOMINAL, RREF);
   float temp_2 = thermo_2.temperature(RNOMINAL, RREF);
@@ -55,33 +51,31 @@ void loop()
 
   //Print the current time 
   time_t t = now();
-  Serial.print("Time= ");Serial.print(hour(t));Serial.print(":");Serial.print(min(t));Serial.print(":");Serial.println(second(t));
+  Serial.print("Run_Duration = ");Serial.print(hour(t));Serial.print(":");Serial.print(minute(t));Serial.print(":");Serial.println(second(t));
   
   //Print temps to serial monitor
   Serial.print("Temp_1 = "); Serial.println(temp_1);
   Serial.print("Temp_2 = "); Serial.println(temp_2);
   Serial.print("Temp_3 = "); Serial.println(temp_3);
   Serial.print("Temp_4 = "); Serial.println(temp_4);
-  
+
   //Check if any of the boards had faults
-  faultPrint(thermo_1, 1);
-  faultPrint(thermo_2, 2);
-  faultPrint(thermo_3, 2);
-  faultPrint(thermo_4, 4);
-  
-  delay(DELAY_TIME_MS)
+  faultCheck();  
+
+  //Delay before next iteration
+  delay(DELAY_TIME_MS);
 }
 
 
 //Check if the passed board had any faults and print them
-//@param thermo : the board object
-//@param thermoNum : a unique identifer for the board for output labelling purposes
-void faultPrint(Adafruit_MAX3185 thermo, int thermoNum)
+//Since we don't have an array of boards, repeats code to check for faults with each board
+void faultCheck()
 {
+  
  // Check and print any faults
-  uint8_t fault = thermo.readFault();
+  uint8_t fault = thermo_1.readFault();
   if (fault) {
-    Serial.print("Fault with board # "); Serial.println(thermoNum);
+    Serial.println("Fault with board 1");
     Serial.print("Fault 0x"); Serial.println(fault, HEX);
     if (fault & MAX31865_FAULT_HIGHTHRESH) {
       Serial.println("RTD High Threshold"); 
@@ -93,7 +87,7 @@ void faultPrint(Adafruit_MAX3185 thermo, int thermoNum)
       Serial.println("REFIN- > 0.85 x Bias"); 
     }
     if (fault & MAX31865_FAULT_REFINHIGH) {
-      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+      Serial.println("REFIN- < 0.85 xBias - FORCE- open"); 
     }
     if (fault & MAX31865_FAULT_RTDINLOW) {
       Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
@@ -101,7 +95,82 @@ void faultPrint(Adafruit_MAX3185 thermo, int thermoNum)
     if (fault & MAX31865_FAULT_OVUV) {
       Serial.println("Under/Over voltage"); 
     }
-    thermo.clearFault();
+    thermo_1.clearFault();
+  }
+
+  fault = thermo_2.readFault();
+  if (fault) {
+    Serial.println("Fault with board 2");
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 xBias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo_2.clearFault();
+  }
+
+  fault = thermo_3.readFault();
+  if (fault) {
+    Serial.println("Fault with board 3");
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 xBias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo_3.clearFault();
+  }
+
+   fault = thermo_4.readFault();
+  if (fault) {
+    Serial.println("Fault with board 4");
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 xBias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo_4.clearFault();
   }
 }
 
